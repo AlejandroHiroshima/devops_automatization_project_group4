13fc5e3e4db0c3358083c0c9aad3c8bfefed4e03
Alexander Hrachovina <alexander.hrachovina@stud.sti.se>
2025-11-12 17:33:58 +0100

    terraform probably done

---
+++ terraform/variables_and_locals.tf
@@ -0,0 +1,53 @@
+variable "location" {
+  description = "Azure region for resources"
+  type        = string
+  default     = "swedencentral"
+}
+
+variable "prefix_app_name" {
+  description = "Name prefix for resources"
+  type        = string
+  default     = "AH_DevOps_Pipeline"
+}
+
+variable "owner" {
+  description = "Name of owner"
+  type        = string
+  default     = "Alexander Hrachovina"
+}
+
+variable "subscription_id" {
+  description = "Azure subscription ID"
+  type        = string
+  default     = "29a87bec-b199-436d-a319-1bc66a3a8c30"
+}
+
+resource "random_string" "suffix" {
+  length  = 3
+  special = false
+  upper   = false
+}
+
+locals {
+  common_tags = {
+    owner = var.owner
+  }
+}
+
+variable "docker_repo" {
+    description = "name of docker repo"
+    type = string
+    default = "alejandrohiroshima/alejandro:"
+}
+
+variable "docker_tag" {
+    description = "image tag to deploy"
+    type = string
+    default = "latest"
+}
+
+variable "docker_url" {
+    description = "URL for docker hub"
+    type = string
+    default = "https://index.docker.io"
+}
